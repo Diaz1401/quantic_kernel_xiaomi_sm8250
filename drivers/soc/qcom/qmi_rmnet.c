769ca220dca9b94a3cc6536374e8a2b163b1a987
Diaz1401 <reagor8161@outlook.com>
2022-02-05 15:56:18 +0700

    drivers: qmi_rmnet: Remove unused function

--- drivers/soc/qcom/qmi_rmnet.c
+++ drivers/soc/qcom/qmi_rmnet.c
@@ -1155,14 +1155,6 @@ static void dfc_wakelock_acquire(struct qmi_info *qmi)
 	}
 }
 
-static void dfc_wakelock_release(struct qmi_info *qmi)
-{
-	if (qmi && qmi->wakelock_active) {
-		__pm_relax(qmi->ws);
-		qmi->wakelock_active = false;
-	}
-}
-
 static void qmi_rmnet_check_stats(struct work_struct *work)
 {
 	struct rmnet_powersave_work *real_work;
